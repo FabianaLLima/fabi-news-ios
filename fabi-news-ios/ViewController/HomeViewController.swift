2c15fd0b404cf9eb596812ac9ba79aaea4e0b266
Anderson Chagas <anderson.chagas@luizalabs.com>
2022-05-18 17:59:57 -0300

    configuracao da table view

---
+++ fabi-news-ios/ViewController/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  fabi-news-ios
+//
+//  Created by Anderson Chagas on 16/05/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
